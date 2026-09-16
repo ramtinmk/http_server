192bc47ece32619da72f462b2ec816f8e9f526bb
ramtinmk <ramtinmk1382@gmail.com>
2026-09-16 13:15:33 +0330

    fixed some gitignore

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ramtinmk/http_server_C
-# Build directory: /home/ramtinmk/http_server_C
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/ramtinmk/http_server_C/bin/run_tests")
-set_tests_properties(unit_tests PROPERTIES  WORKING_DIRECTORY "/home/ramtinmk/http_server_C" _BACKTRACE_TRIPLES "/home/ramtinmk/http_server_C/CMakeLists.txt;74;add_test;/home/ramtinmk/http_server_C/CMakeLists.txt;0;")
